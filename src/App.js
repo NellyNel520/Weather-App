1fa8eb5b45c5e693d555222661f25c9eb74adc2a
Nelly <$>
2022-12-10 00:18:31 -0500

    rendering desired data from open weather api

--- src/App.js
+++ src/App.js
@@ -27,27 +27,27 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Bronx</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-          <h1>60°F</h1>
+          {data. main ? <h1>{data.main.temp}°F</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         <div className="bottom">
           <div className="feels">
-            <p className="bold">50°F</p>
+            {data. main ? <p className="bold">{data.main.feels_like}°F</p> : null}
             <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p className="bold">20%</p>
+            {data. main ? <p className="bold">{data.main.humidity}%</p> : null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">10 MPH</p>
+          {data. wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
             <p>Wind Speed</p>
           </div>
         </div>
